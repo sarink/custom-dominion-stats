31b08bb47972a6cf47e1986d8e13086642f31642
Kabir Sarin <sarink87@gmail.com>
2016-09-13 08:44:12 -0700

    Add ability to queue up multiple dates via command line args or modify the dateStrings variable

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-(function(dateString) {
+(function(dateStrings) {
 
   var fs = require('fs');
   var mkdirp = require('mkdirp');
@@ -6,8 +6,9 @@
   var Promise = require('bluebird');
   var request = require('request');
   var _ = require('lodash');
+  var leftPad = require('left-pad');
 
-  var MAX_CONCURRENT_AJAX_REQUESTS = 50;
+  var MAX_CONCURRENT_AJAX_REQUESTS = 20;
 
   var writeGameLogToFile = function(dir, file, contents) {
     mkdirp(dir, function(errorCreatingDirectory) {
@@ -54,37 +55,58 @@
     });
   };
 
-  var downloadGameLogsForDate = function(yyyymmdd) {
+  var downloadAndWriteRelevantGameLogsForDate = function(yyyymmdd) {
     var baseUrl = 'http://dominion-game-logs.s3.amazonaws.com/';
     var gameLogsUrl = baseUrl + yyyymmdd + '/index.html';
 
     console.log('fetching game logs for date:', yyyymmdd);
-    request(gameLogsUrl, function(error, response, body) {
-      var logUrls = [];
+    return new Promise(function(resolve, reject) {
+      request(gameLogsUrl, function(error, response, body) {
+        var logUrls = [];
 
-      var hasErrored = error || body.indexOf('<Error>') !== -1;
-      if (!hasErrored) {
-        var $ = cheerio.load(body);
-        $('a').each(function() {
-          var $a = $(this);
-          var logUrl = $a.attr('href');
-          if (logUrl.indexOf('://dominion-game-logs.s3.amazonaws.com/game_logs/' + yyyymmdd) !== -1) {
-            logUrls.push(logUrl);
-          }
-        });
+        var hasErrored = error || body.indexOf('<Error>') !== -1;
+        if (!hasErrored) {
+          var $ = cheerio.load(body);
+          $('a').each(function() {
+            var $a = $(this);
+            var logUrl = $a.attr('href');
+            if (logUrl.indexOf('://dominion-game-logs.s3.amazonaws.com/game_logs/' + yyyymmdd) !== -1) {
+              logUrls.push(logUrl);
+            }
+          });
+          console.log('number of game logs for date ' + yyyymmdd + ':', logUrls.length);
 
-        console.log('number of logs for date ' + yyyymmdd + ':', logUrls.length);
+          return resolve(Promise.map(logUrls, fetchGameLog, {concurrency: MAX_CONCURRENT_AJAX_REQUESTS,}).then(function() {
+            console.log('done fetching all game logs for date:', yyyymmdd);
+            console.log('------------------------------------------------------------');
+          }));
 
-        Promise.map(logUrls, fetchGameLog, {concurrency: MAX_CONCURRENT_AJAX_REQUESTS,}).then(function() {
-          console.log('done fetching all game log urls!');
-        });
-
-      } else {
-        console.log('error fetching game log urls');
-      }
-
+        } else {
+          console.log('error fetching game log urls for date:', yyyymmdd);
+        }
+      });
     });
   };
 
-  downloadGameLogsForDate(dateString);
-}(process.argv.slice(2)[0]));
+  if (_.isEmpty(dateStrings)) {
+    dateStrings = [];
+    for (var year = 2016; year <= 2016; year++) {
+      var yearStr = year.toString();
+      for (var month = 9; month <= 9; month++) {
+        var monthStr = leftPad(month, 2, 0);
+        for (var day = 1; day <= 12; day++ ) {
+          var dayStr = leftPad(day, 2, 0);
+          var dateString = yearStr + monthStr + dayStr;
+          dateStrings.push(dateString);
+        }
+      }
+    }
+  }
+
+  console.log('executing with dateStrings:', dateStrings);
+
+  Promise.map(dateStrings, downloadAndWriteRelevantGameLogsForDate, {concurrency: 1,}).then(function() {
+    console.log('finished downloading and writing game logs for all dateStrings');
+    process.exit();
+  });
+}(process.argv.slice(2)));
