62e91ba1db60e96827ac3e68e11ce0efe3dfd009
Kabir Sarin <sarink87@gmail.com>
2016-09-22 01:58:27 -0700

    Little bit of styling to LastUpdatedStats, remove inline styles

--- client/javascripts/LastUpdatedStats.js
+++ client/javascripts/LastUpdatedStats.js
@@ -5,14 +5,13 @@ window.App.LastUpdatedStats = (function() {
   class LastUpdatedStats extends Component {
     render() {
       const { lastGitPull, lastDbUpdate, lastDbLogUrl } = this.props;
-      const style = { float: 'right' };
       const lastGitPullDate = new Date(lastGitPull);
       const lastDbUpdateDate = new Date(lastDbUpdate);
       return (
-        <div style={style}>
+        <div className="lastUpdatedStats">
           <div>Last git pull: {lastGitPullDate.toString()}</div>
           <div>Last db update: {lastDbUpdateDate.toString()}</div>
-          <div>Last db log url: <a target="_blank" href={lastDbLogUrl}>{lastDbLogUrl}</a></div>
+          <div className="lastUpdatedStats-lastDbLogUrl">Last db log url: <a target="_blank" href={lastDbLogUrl}>{lastDbLogUrl}</a></div>
         </div>
       );
     }
