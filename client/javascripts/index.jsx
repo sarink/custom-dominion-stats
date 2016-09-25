243749af72bb36fac62e12d9b8537c545eda289f
Pete Schlette <pete.schlette@gmail.com>
2016-09-25 02:27:28 +0000

    Add jsx transforming

--- client/javascripts/index.jsx
+++ client/javascripts/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // const { RootContainer } = window.App;
-// ReactDOM.render(<div />, document.getElementById('rootContainer'));
+ReactDOM.render(<div />, document.getElementById('rootContainer'));
