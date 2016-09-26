9561fc93a7dbb1416d2289a22c7daa3dfbadaa15
Kabir Sarin <sarink87@gmail.com>
2016-09-25 23:47:51 -0700

    Remove filtering from the Root

--- client/javascripts/Root.jsx
+++ client/javascripts/Root.jsx
@@ -7,54 +7,26 @@ import GameExplorer from 'javascripts/GameExplorer';
 import LastUpdatedStats from 'javascripts/LastUpdatedStats';
 
 export default class Root extends Component {
-  componentWillMount() {
-    this.setState({
-      playerNames: ['sarink','nisse038','cherrypeel'],
-      filteredGames: this.props.allGames,
-    });
-  }
-
-  // TODO ok, obviously this is terrible
-  componentDidMount() {
-    this.handlePlayerNamesChange({target: {value: this.state.playerNames.join(',')}});
-  }
-
-  handlePlayerNamesChange = (event) => {
-    if (_.isEmpty(event.target.value)) {
-      this.setState({playerNames: '', filteredGames: this.props.allGames});
-    } else {
-      const playerNames = event.target.value.split(',');
-      const filteredGames = this.props.allGames.filter(game => {
-        return _.difference(game.playerList, playerNames).length === 0 && game.playerList.length === playerNames.length;
-      });
-      this.setState({playerNames, filteredGames});
-    }
-  }
-
   render() {
     const { allGames, lastGitPull, lastDbUpdate, lastDbLogUrl } = this.props;
-    const { playerNames, filteredGames } = this.state;
 
     let content = null;
 
-    const showGameExplorer = !_.isEmpty(filteredGames);
-    const showLeaderboard = !_.isEmpty(filteredGames);
+    const showGameExplorer = !_.isEmpty(allGames);
+    const showLeaderboard = !_.isEmpty(allGames);
     const showLastUpdatedStats = !_.isEmpty(lastGitPull) || !_.isEmpty(lastDbUpdate) || !_.isEmpty(lastDbLogUrl);
     if (!showGameExplorer && !showLeaderboard) {
       content = 'Nothing to display :(';
     } else {
       content = [
-        showLeaderboard ? <Leaderboard key="leaderboard" highlightPlayers={playerNames ? playerNames.join(',') : ''} games={filteredGames} /> : null,
-        showGameExplorer ? <GameExplorer key="gameExplorer" games={filteredGames} /> : null,
+        showLeaderboard ? <Leaderboard key="leaderboard" games={allGames} /> : null,
+        showGameExplorer ? <GameExplorer key="gameExplorer" games={allGames} /> : null,
       ];
     }
 
     return (
       <div className="root">
         <div className="root-content">
-          <div className="root-globalFilters">
-            Player names (AND, ex: player1,player2,etc): <input onChange={this.handlePlayerNamesChange} type="text" value={this.state.playerNames} />
-          </div>
           {content}
         </div>
         <div className="root-lastUpdatedStats">
